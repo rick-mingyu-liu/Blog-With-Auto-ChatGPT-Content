1f5971121d0250cceed9f2dc20329eb73bee085f
rick-mingyu-liu <rickmyliu@gmail.com>
2024-10-31 09:57:55 +0800

    link the trending post's title to the blog

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,45 @@ import Travel from "./(home)/Travel";
 import Other from "./(shared)/Other";
 import Subscribe from "./(shared)/Subscribe";
 import Sidebar from "./(shared)/Sidebar";
+import { prisma } from "./api/client"; // the app api
+import { Post } from "@prisma/client";
 
-export default function Home() {
+const getPosts = async () => {
+  const posts: Array<Post> = await prisma.post.findMany();
+  return posts;
+}
+
+export default async function Home() {
+  const posts = await getPosts();
+  console.log(posts);
+
+  const formattedPosts = () => {
+    const trendingPosts: Array<Post> = [];
+    const techPosts: Array<Post> = [];
+    const travelPosts: Array<Post> = [];
+    const otherPosts: Array<Post> = [];
+
+    posts.forEach((post: Post, i: number) => {
+      if (i < 4) {
+        trendingPosts.push(post);
+      }
+      if (post?.category === "Tech") {
+        techPosts.push(post);
+      } else if (post?.category === "Travel") {
+        travelPosts.push(post);
+      } else if (post?.category === "Interior Design") {
+        otherPosts.push(post);
+      }
+    });
+
+    return [travelPosts, techPosts, travelPosts, otherPosts];
+  };
+
+  const [trendingPosts, techPosts, travelPosts, otherPosts] = formattedPosts();
+
   return (
     <main className="px-10 leading-7">
-        <Trending />
+        <Trending trendingPosts={trendingPosts}/>
         <div className="md:flex gap-10 mb-5">
           <div className="basis-3/4">
             <Tech />

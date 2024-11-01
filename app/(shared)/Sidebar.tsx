d6f4a1e04ef1f98ab446b4a36cc36d67dff51687
rick-mingyu-liu <rickmyliu@gmail.com>
2024-11-01 10:17:02 +0800

    sidebar image implement

--- app/(shared)/Sidebar.tsx
+++ app/(shared)/Sidebar.tsx
@@ -1,12 +1,11 @@
 import React from "react";
-import SocialLinks from "./socialLinks";
+import SocialLinks from "./SocialLinks";
 import Subscribe from "./Subscribe";
+import Image from "next/image";
+import Ad2 from "../../public/assets/ad-2.png";
+import AboutProfile from "../../public/assets/about-profile.jpg";
 
-type Props = {
-
-}
-
-const Sidebar = (props: Props) => {
+const Sidebar = () => {
     return (
         <section>
             <h4 className="bg-wh-900 py-3 px-5 text-wh-50 text-xs font-bold text-center">
@@ -16,11 +15,27 @@ const Sidebar = (props: Props) => {
                 <SocialLinks isDark />
             </div>
             <Subscribe />
-            <div className="bg-wh-900 my-8">advert image</div>
-            <h4 className="bg-wh-900 py-3 px-5 text-wh-50 text-xs font-bold text-center">
-                About the Blog
-            </h4>
-            <div className="bg-wh-900 my-8">profile image</div>
+            <div className="bg-wh-900 my-8">
+                <Image
+                    className="hidden md:block my-8 w-full"
+                    alt="advert-2"
+                    placeholder="blur"
+                    src={Ad2}
+                    width={500}
+                    height={1000}
+                />
+            </div>
+            <h4 className="bg-wh-900 py-3 px-5 text-wh-50 text-xs font-bold text-center">About the Blog</h4>
+            <div className="flex justify-center my-3">
+                <Image
+                    className="hidden md:block my-8 w-full"
+                    alt="about-profile"
+                    placeholder="blur"
+                    src={AboutProfile}
+                    width={500}
+                    height={1000}
+                />
+            </div>
             <h4 className="py-3 px-5 text-wh-500 font-bold text-center">
                 Geoffrey Epstein
             </h4>

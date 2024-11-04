820fb2c989c205b2b2cef3483121bc61a0c3ee6b
rick-mingyu-liu <rickmyliu@gmail.com>
2024-11-04 16:36:22 +0800

    rich content edit section finish

--- app/post/[id]/Content.tsx
+++ app/post/[id]/Content.tsx
@@ -124,6 +124,7 @@ const Content = ({ post }: Props) => {
                                 onChange={handleOnChangeTitle}
                                 value={title}
                             />
+                            {titleError && <p className="mt-1 text-primary-500">{titleError}</p>}
                         </div>
                     ) : (
                         <h3 className="font-bold text-3xl mt-3">

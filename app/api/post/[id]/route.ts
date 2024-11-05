9dd1171456d800565649db4a422d405b62ca42b4
rick-mingyu-liu <rickmyliu@gmail.com>
2024-11-05 14:55:18 +0800

    final attempt

--- app/api/post/[id]/route.ts
+++ app/api/post/[id]/route.ts
@@ -1,18 +1,19 @@
 import { NextResponse } from "next/server";
 import { prisma } from "../../client";
-import type { NextRequest } from "next/server";
 
-export async function PATCH(request: NextRequest, context: any) {
+type Params = { params: { id: string } };
+
+export async function PATCH(request: Request, { params }: Params) {
   try {
-    const { id } = context.params; // Access params directly
+    const { id } = params;
     const { title, content } = await request.json();
     const post = await prisma.post.update({
-      where: { id },
+      where: { id: id },
       data: { title, content },
     });
     return NextResponse.json(post, { status: 200 });
   } catch (error) {
     console.error("request error", error);
-    return NextResponse.json({ error: "error updating post" }, { status: 500 });
+    NextResponse.json({ error: "error updating post" }, { status: 500 });
   }
 }
